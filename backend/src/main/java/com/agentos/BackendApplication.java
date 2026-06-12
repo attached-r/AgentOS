69da4631d3be2ff2ce35e25c12669b7fde7005dc
attached <2165329383@qq.com>
2026-06-12 17:31:26 +0800

    完成对sotoken的引入测试 配置相应yaml设置

--- backend/src/main/java/com/agentos/BackendApplication.java
+++ backend/src/main/java/com/agentos/BackendApplication.java
@@ -1,8 +1,9 @@
 package com.agentos;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.agentos.mapper")   // 扫描mapper
 @SpringBootApplication
 public class BackendApplication {
 
